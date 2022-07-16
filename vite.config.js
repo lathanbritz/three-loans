19efb03fd6262f104fc4639a8bea26cbc0d9df29
lathan britz <lathan.britz@drupalconnect.com>
2022-07-15 21:40:36 -0400

    give it a bash ott fetch

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import fs from 'fs'
 
 // https://vitejs.dev/config/
-const config = (process.env.NODE_ENV === 'production') ? {
+const config = (import.meta.env.NODE_ENV === 'production') ? {
   plugins: [vue()],
   server: {
     https: {
