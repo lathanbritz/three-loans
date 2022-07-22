158a92c14f125b8174d46704c6554e327cb1bc4a
lathan britz <lathan.britz@drupalconnect.com>
2022-07-21 21:25:29 -0400

    put back router leave menu removesd

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from "vue-router"
+
+import Home from "../views/Home.vue"
+
+export const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+        props: { msg: "Escrows" },
+    }
+];
+
+const history = createWebHistory()
+
+const router = createRouter({
+    history,
+    routes,
+})
+
+export default router
