b99648c95bc3445e13ae70b71d1c55b555a2c435
lathan britz <lathan.britz@drupalconnect.com>
2022-07-23 12:33:08 -0400

    certs work

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import fs from 'fs'
+import mkcert from'vite-plugin-mkcert'
 
-export default defineConfig({
-  plugins: [vue()],
+// import fs from 'fs'
+
+// export default defineConfig({
+//   plugins: [vue()],
+//   server: {
+//     https: {
+//       key: fs.readFileSync('../../ssl/panicbot.xyz.key'),
+//       cert: fs.readFileSync('../../ssl/panicbot_xyz.crt'),
+//     },
+//   },
+//   define: {
+//     'process.env': {}
+//   }
+// })
+
+export default defineConfig({ 
   server: {
-    https: {
-      key: fs.readFileSync('../../ssl/panicbot.xyz.key'),
-      cert: fs.readFileSync('../../ssl/panicbot_xyz.crt'),
-    },
+    https: true
   },
-  define: {
-    'process.env': {}
-  }
+  plugins: [vue(), mkcert()] 
 })
-
-// export default defineConfig({ plugins: [vue()] })
 
