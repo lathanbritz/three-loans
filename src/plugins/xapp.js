d9603d1dc13747f37b957ea3bc22794ac84b84ea
lathan britz <lathan.britz@drupalconnect.com>
2022-07-23 17:32:48 -0400

    certs work

--- src/plugins/xapp.js
+++ src/plugins/xapp.js
@@ -94,34 +94,6 @@ const status = () => {
     })
 }
 
-const getPayload = async (payload_uuid) => {
-    try {
-        console.log('url', `https://xumm.app/api/v1/platform/payload/${payload_uuid}`)
-        console.log('head', {headers: { 'Accept': 'application/json',  'X-API-Key': apiKey, 'X-API-Secret': apiSecret }})
-        //  curl --request GET      
-        // --url https://xumm.app/api/v1/platform/payload/5b1c847c-15f5-4ce2-882d-d9fca6126600   
-        // --header 'Accept: application/json'      
-        // --header 'X-API-Key: fa0382d0-ed38-458b-b0a1-2972567187de'      
-        // --header 'X-API-Secret: xxxxxx'
-
-        const result = await axios.get(`https://xumm.app/api/v1/platform/payload/${payload_uuid}`, { 
-        // const result = await axios.get(`${apiEndPoint}/payload/${payload_uuid}`, {     
-            headers: { 
-                'Accept': 'application/json',  
-                'X-API-Key': apiKey, 
-                'X-API-Secret': apiSecret,
-                'Access-Control-Allow-Headers':'X-API-Key',
-                'Access-Control-Allow-Headers':'X-API-Secret',
-            } 
-        })
-
-        return result
-    } catch(e) {
-        console.log('response e', e.response)
-        if (e === '') throw { msg: 'closed', error: false }
-        throw e
-    }
-}
 const payload = async (payload) => {
     try {
         const res = await axios.post(`${apiEndPoint}/payload`, payload, headers())
@@ -177,7 +149,6 @@ export default {
     getTokenData,
     closeXapp,
     signPayload: payload,
-    getPayload,
     openExternalBrowser,
     openTxViewer,
     versionCheck
